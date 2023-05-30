55a6771646cd40ff8e0da857b84ec3421006e91e
deepak <deepak.k@antino.io>
2023-05-30 15:43:51 +0530

    worked with file system

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const http=require("http");
+const fs=require("fs");
 const port =8000;
+fs.readFile('info.txt','utf8',(err,data)=>{
+    console.log(data);
+})
+fs.writeFile("demo.txt","this is demo text",(err,data)=>   {
+    console.log(err);
+})
 const server=http.createServer((req,res)=>{
     res.statusCode=200; 
      res.setHeader("Content-type","text/html");
